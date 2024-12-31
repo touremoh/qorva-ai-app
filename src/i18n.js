7bc3d22d971141b44d7da20b52964c1ff23ff157
Mohamed Toure <demahom.toure@gmail.com>
2024-12-31 03:06:11 +0100

    Makes the browser primary language as default navigation language

--- src/i18n.js
+++ src/i18n.js
@@ -11,20 +11,35 @@ import ptTranslation from './locales/pt/translation.json';
 import itTranslation from './locales/it/translation.json';
 import nlTranslation from './locales/nl/translation.json';
 
+// Function to get the browser language and map it to available translations
+const getBrowserLanguage = () => {
+	const browserLang = navigator.language || navigator.languages[0];
+	const supportedLanguages = ['en', 'fr', 'de', 'es', 'pt', 'it', 'nl'];
+
+	// Extract primary language code (e.g., 'en' from 'en-US')
+	const primaryLang = browserLang.split('-')[0];
+
+	// Log the primary language of the user
+	console.log('Primary language: ', primaryLang);
+
+	// Return primary language if supported, else fallback to 'en'
+	return supportedLanguages.includes(primaryLang) ? primaryLang : 'en';
+};
+
 i18n
 	.use(initReactI18next)
 	.init({
 		resources: {
-			en: {translation: enTranslation},
-			fr: {translation: frTranslation},
-			de: {translation: deTranslation},
-			es: {translation: esTranslation},
-			pt: {translation: ptTranslation},
-			it: {translation: itTranslation},
-			nl: {translation: nlTranslation},
+			en: { translation: enTranslation },
+			fr: { translation: frTranslation },
+			de: { translation: deTranslation },
+			es: { translation: esTranslation },
+			pt: { translation: ptTranslation },
+			it: { translation: itTranslation },
+			nl: { translation: nlTranslation },
 		},
-		lng: 'en', // default language
-		fallbackLng: 'en', // fallback language if a translation is missing
+		lng: getBrowserLanguage(), // Set default language to the browser's language
+		fallbackLng: 'en', // Fallback language if a translation is missing
 		interpolation: {
 			escapeValue: false, // React already escapes values
 		},
